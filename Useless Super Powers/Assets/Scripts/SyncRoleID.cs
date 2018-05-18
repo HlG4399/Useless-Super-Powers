f4d6f4f5c245982252616b0cf5361c230c30c32b
HlG4399 <CaiQinyi@foxmail.com>
2018-05-18 11:12:57 +0800

    同步RoleID变量调试

--- Useless Super Powers/Assets/Scripts/SyncRoleID.cs
+++ Useless Super Powers/Assets/Scripts/SyncRoleID.cs
@@ -8,6 +8,7 @@ public class SyncRoleID : NetworkBehaviour
     [Command]
     public void CmdSetID(int id)
     {
+        Debug.Log("isServer:"+isServer);
         ChooseRole.RoleID = id;
         Debug.Log("服务器端" + ChooseRole.RoleID);
     }
